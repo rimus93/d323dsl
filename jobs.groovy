a518028ac1efb24b0f03c8eac13bd36e8b6a4219
rimus93 <rimus93@yandex.ru>
2020-04-08 12:12:40 +0300

    make child jobs in checkbox

---
+++ jobs.groovy
@@ -0,0 +1,19 @@
+job('MNTLAB-meremin-main-build-job') {
+    description 'Main job'
+    parameters {
+    	choiceParam('BRANCH_NAME', ['meremin', 'master'], 'Branch name')
+        activeChoiceParam('BUILD_TRIGGER') {
+            choiceType('CHECKBOX')
+            groovyScript {
+                script('''return ["MNTLAB-meremin-child1-build-job", 
+                       "MNTLAB-meremin-child2-build-job", 
+                       "MNTLAB-meremin-child3-build-job", 
+                       "MNTLAB-meremin-child4-build-job"]
+						''')
+                fallbackScript('"fallback choice"')
+            }
+        }
+    }
+}
+
+
