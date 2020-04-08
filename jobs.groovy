bd539d9ae84e345c59e768ea479895bfee339c9b
rimus93 <rimus93@yandex.ru>
2020-04-08 13:07:18 +0300

    make git, shell and artifact

--- jobs.groovy
+++ jobs.groovy
@@ -30,4 +30,32 @@ job('MNTLAB-meremin-main-build-job') {
     }
 }
 
+for (i in 1..4) {
+   job("MNTLAB-meremin-child${i}-build-job") {
+     	scm {
+           git {
+           		remote {
+                	url('https://github.com/rimus93/d323dsl.git')
+            	}
+            }
+       
+    	}
+   		parameters {
+        	gitParam('BRANCH_NAME') {
+            	type('BRANCH')
+            }
+    	}
+        steps {
+            shell('''chmod +x script.sh
+                ./script.sh > output.txt
+tar -czvf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy''')
+        }
+     	publishers {
+			archiveArtifacts {
+              pattern('${BRANCH_NAME}_dsl_script.tar.gz')
+            }
+        }
+     
+    }  
+}
 
